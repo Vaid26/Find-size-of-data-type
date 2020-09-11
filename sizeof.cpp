687e5ab4c10ac37b462e5304e7ac56d592da067f
Vaid26 <67861541+Vaid26@users.noreply.github.com>
2020-09-12 00:42:24 +0530

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a;
+float b;
+char r;
+double e;
+cout<<"the size of int is:"<<sizeof(a)<<"\n";
+cout<<"the size of float is:"<<sizeof(b)<<"\n";
+cout<<"the size of char is:"<<sizeof(r)<<"\n";
+cout<<"the size of double is:"<<sizeof(e)<<"\n";
+return 0;
+}
